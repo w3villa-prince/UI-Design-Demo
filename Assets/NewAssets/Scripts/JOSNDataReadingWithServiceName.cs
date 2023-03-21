1eb627b68d01c77a6e4100d3f020e9e2101c70dd
w3villa-prince <prince.yadav@w3villa.com>
2023-03-21 12:51:35 +0530

    WebGL Build For W3Villa VR Demo Scence

--- Assets/NewAssets/Scripts/JOSNDataReadingWithServiceName.cs
+++ Assets/NewAssets/Scripts/JOSNDataReadingWithServiceName.cs
@@ -5,12 +5,21 @@ public class JOSNDataReadingWithServiceName : MonoBehaviour
     public TextAsset textJOSN;
     private ServiceManager _ServiceManagerList;
 
+    public Vector3 _3DHallposition = new Vector3(0, 0, 0);
+
+    public Vector3 _3dPosterStartingPostion = new Vector3(-4, 0, 8);
+    public Vector3 first_3DPosterPosition = new Vector3(12, 0, 0);
+
+    public Vector3 second_3DPosterPosition = new Vector3(-12, 0, 4);
+    public Vector3 _3DHallpositionUpdate = new Vector3(25, 0, 0);
+
     [System.Serializable]
     public class SubService
     {
         public string name;
         public string imageLink;
         public string actionLink;
+        public string description;
     }
 
     [System.Serializable]
@@ -44,39 +53,39 @@ public class JOSNDataReadingWithServiceName : MonoBehaviour
 
     private void DataInflator()
     {
-        Vector3 position = new Vector3(-6, 0, 0);
+        // Vector3 position = new Vector3(-6, 0, 0);
         foreach (var subserivce in serivceList.services)
         {
             // i++;
             //Debug.Log("Sub service " + i);
-            _ServiceManagerList.CreateService(position, subserivce.serviceName);
+            _ServiceManagerList.CreateService(_3DHallposition, subserivce.serviceName);
 
             //_subServicesViewList.GetComponent<SubServicesView>().cr;
 
-            Vector3 postionOfSubService = position + new Vector3(-4, 0, 8);
+            Vector3 postionOfSubService = _3DHallposition + _3dPosterStartingPostion;
             int i = 1;
 
-            float rotateValue = 135;
+            float rotateValue = 0;
             foreach (var view in subserivce.subService)
             {
-                _ServiceManagerList.CreateSubSerivcePrefabs(view.name, " ", view.imageLink, view.actionLink, postionOfSubService, rotateValue);
+                _ServiceManagerList.CreateSubSerivcePrefabs(view.name, view.description, view.imageLink, view.actionLink, postionOfSubService, rotateValue);
 
                 if (i % 2 != 0)
                 {
-                    postionOfSubService += new Vector3(12, 0, 0);
-                    rotateValue = 45;
+                    postionOfSubService += first_3DPosterPosition;
+                    rotateValue = 180;
                 }
                 else
                 {
-                    postionOfSubService += new Vector3(-12, 0, 4);
-                    rotateValue = 135;
+                    postionOfSubService += second_3DPosterPosition;
+                    rotateValue = 0;
                 }
 
                 i++;
                 //  GetComponentInChildren<ServiceViewUpdate>().UpdateServiceSectionItemData(view.name, " ", view.imageLink, view.actionLink);
                 // Debug.Log(view.name);
             }
-            position += new Vector3(25, 0, 0);
+            _3DHallposition += _3DHallpositionUpdate;
         }
     }
 
